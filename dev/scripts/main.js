9e33628a63ffdc8c07803aa3fb2cfb97200b6121
puck3000 <daniel.lerch@gmail.com>
2015-02-12 11:22:18 +0100

    perfecting the new Head

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,9 +1,11 @@
 $(function() {  
    $("header").headroom();
 });
-// transformicons
-!function(t,n){"function"==typeof define&&define.amd?define(n):t.transformicons=n()}(this||window,function(){"use strict";var t={},n="tcon-transform",r={transform:["click","touchstart"],revert:["click","touchstart"]},e=function(t){return"string"==typeof t?Array.prototype.slice.call(document.querySelectorAll(t)):"undefined"==typeof t||t instanceof Array?t:[t]},o=function(t){return"string"==typeof t?t.toLowerCase().split(" "):t},c=function(t,n,c){var f=(c?"remove":"add")+"EventListener",u=e(t),a=u.length,s={};for(var l in r)s[l]=n&&n[l]?o(n[l]):r[l];for(;a--;)for(var d in s)for(var v=s[d].length;v--;)u[a][f](s[d][v],i)},i=function(n){t.toggle(n.currentTarget)};return t.add=function(n,r){return c(n,r),t},t.remove=function(n,r){return c(n,r,!0),t},t.transform=function(r){return e(r).forEach(function(t){t.classList.add(n)}),t},t.revert=function(r){return e(r).forEach(function(t){t.classList.remove(n)}),t},t.toggle=function(r){return e(r).forEach(function(r){t[r.classList.contains(n)?"revert":"transform"](r)}),t},t});transformicons.add('.tcon');
 
+// transformicons
+!function(n,r){"function"==typeof define&&define.amd?define(r):n.transformicons=r()}(this||window,function(){"use strict";var n={},r="tcon-transform",t={transform:["click"],revert:["click"]},e=function(n){return"string"==typeof n?Array.prototype.slice.call(document.querySelectorAll(n)):"undefined"==typeof n||n instanceof Array?n:[n]},o=function(n){return"string"==typeof n?n.toLowerCase().split(" "):n},i=function(n,r,i){var c=(i?"remove":"add")+"EventListener",u=e(n),a=u.length,s={};for(var l in t)s[l]=r&&r[l]?o(r[l]):t[l];for(;a--;)for(var d in s)for(var v=s[d].length;v--;)u[a][c](s[d][v],f)},f=function(r){n.toggle(r.currentTarget)};return n.add=function(r,t){return i(r,t),n},n.remove=function(r,t){return i(r,t,!0),n},n.transform=function(t){return e(t).forEach(function(n){n.classList.add(r)}),n},n.revert=function(t){return e(t).forEach(function(n){n.classList.remove(r)}),n},n.toggle=function(t){return e(t).forEach(function(t){n[t.classList.contains(r)?"revert":"transform"](t)}),n},n});transformicons.add('.tcon');
+
+// mobile Nav toggle
 $(function() {  
     $('#navToggle').click(function() {
     	$('nav').toggleClass('open');
